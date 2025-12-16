8925d53f96976c95ae3ba2fa93a2cd9275ec212b
jerem p <palette.jeremie@live.fr>
2025-12-16 17:52:28 +0100

    fix formTest

--- src/containers/Form/index.test.js
+++ src/containers/Form/index.test.js
@@ -14,16 +14,29 @@ describe("When Events is created", () => {
     it("the success action is called", async () => {
       const onSuccess = jest.fn();
       render(<Form onSuccess={onSuccess} />);
-      fireEvent(
-        await screen.findByTestId("button-test-id"),
-        new MouseEvent("click", {
-          cancelable: true,
-          bubbles: true,
-        })
-      );
-      await screen.findByText("En cours");
+      
+      // je récup tous les champs qui ont le même test-id
+      const fields = screen.getAllByTestId("field-testid");
+
+      // je les remplit un par un en suivant l'ordre du HTML
+      // index 0 : nom (lastname)
+      fireEvent.change(fields[0], { target: { value: "NomTest"} });
+      // index 1 : prénom (firstname)
+      fireEvent.change(fields[1], { target: { value: "PrenomTest"} });
+      // index 2 : Email
+      fireEvent.change(fields[2], { target: { value: "test@test.com"} });
+      // index 3 : Message (textarea)
+      fireEvent.change(fields[3], { target: { value: "Ceci est un message de test"} });
+        
+      // l'event clic sur le btn
+      fireEvent.click(screen.getByTestId("button-test-id"));
+
+      await screen.findByText(/en cours/i);
       await screen.findByText("Envoyer");
+
       expect(onSuccess).toHaveBeenCalled();
+
+
     });
   });
 });
